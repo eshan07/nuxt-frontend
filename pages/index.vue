34de70309da761e713e7fa117fdd0ac310c55bc0
eshan07 <15103066@iubat.edu>
2022-06-07 09:34:10 +0600

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">Dashboard</h1>
+
+      <p>Hi {{ user.name }}</p>
+
+      <a href="#" @click.prevent="logout">Logout</a>
+    </div>
+  </section>
+</template>
+<script>
+import Card from '~/components/Card'
+
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      user: this.$auth.user.data,
+    }
+  },
+  methods: {
+    async logout() {
+      await this.$auth.logout()
+
+      this.$router.push('/login')
+    },
+  },
+}
+</script>
